52a4214cf722dc08d5896d7116cddb39cdd640b6
Panchenko Vadim <vadimpancs@gmail.com>
2018-09-07 14:57:23 +0300

    Fix type of count

--- countOfVisitings.php
+++ countOfVisitings.php
@@ -1,6 +1,6 @@
 <?php
 
-$count = isset($_COOKIE['count']) ? $_COOKIE['count'] + 1 : 0;
+$count = isset($_COOKIE['count']) ? (int)$_COOKIE['count'] + 1 : 0;
 
 setcookie('count', $count, time()+60*60*24*366);
 
